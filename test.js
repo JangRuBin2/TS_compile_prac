373ad191120813d9d784c82989e42b0a3e3e878c
jangrubin <wkdfnqls2465@gmail.com>
2023-05-17 11:36:00 +0900

    alt + shift + a로 스니펫 만들기

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 // console.log("하이");
 // function first(arrayData) {
 //   // 1.첫번째 배열 인덱스의 변환하는 함수를 만들고 싶다.
@@ -10,16 +19,45 @@
 // first(data);
 //* 매개변수를 배열로 받을 예정
 // * typeof(arrayData) == "string"
-function first(arrayData) {
+function first(arrayData, insertString) {
     // 1.첫번째 배열 인덱스의 변환하는 함수를 만들고 싶다.
     // let result : string = arrayData[0];
     // result = "다른값";
     // let arr : string[] = arrayData.unshift();
     // return arr;
-    var result = "강지민";
-    var arr = [];
-    arr.unshift(result);
-    return arr;
+    // let result : string = "강지민";
+    // let arr : string[] = []
+    // arr.unshift(result);
+    // return arr;
+    // // ! 첫번째
+    // // let kimochi : string = "기모찌";
+    // // let arr : string[] = [];
+    // // arr.push(kimochi);
+    // // // return arr.push(kimochi);
+    // // return arr;
+    // // ! 두번째
+    // // let susi : string = "기모링";
+    // let arr :string[] = [];
+    // let result : string[] = arr.map(a => {
+    //   a + "하이"
+    // })
+    // return result;
+    // * 1
+    // const insertString :string = 
+    // * 2 
+    arrayData.pop();
+    // * 3
+    var combineArray = [];
+    // * 4
+    combineArray = __spreadArray([insertString], arrayData, true);
+    // * 5
+    return combineArray;
 }
+// 기존 배열에 0번째를 최종적으로 바꿔주는 형태
+// - …전개 연산자
+// - reduce()
+// - map()
+// - push()
 var data = ['최대건', '정성철', '허진', '이세민'];
-console.log(first(data));
+console.log(first(data, "대머리"));
+// console.log(...data)
--- test.ts
+++ test.ts
@@ -1,3 +1,13 @@
+/* ALT + SHIFT + A */ 
+/**
+ * @ params {array} arrayData
+ * @ params {string} insertData
+ * @ return {array} 문자열로 된 배열입니다
+ * 
+ * 
+ * 
+ * 
+*/
 // console.log("하이");
 // function first(arrayData) {
 //   // 1.첫번째 배열 인덱스의 변환하는 함수를 만들고 싶다.
