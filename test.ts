ad0387cc19cfde81538cac9cd4e7967ae3c8536e
jangrubin <wkdfnqls2465@gmail.com>
2023-05-17 11:29:40 +0900

    배열 첫번째에 대머리 들어오는 것 확인

--- test.ts
+++ test.ts
@@ -10,7 +10,7 @@
 // first(data);
 //* 매개변수를 배열로 받을 예정
 // * typeof(arrayData) == "string"
-function first(arrayData : string[]) : string[] {
+function first(arrayData : string[], insertString : string) : string[] {
   // 1.첫번째 배열 인덱스의 변환하는 함수를 만들고 싶다.
   // let result : string = arrayData[0];
   // result = "다른값";
@@ -20,11 +20,29 @@ function first(arrayData : string[]) : string[] {
   // let arr : string[] = []
   // arr.unshift(result);
   // return arr;
-  // ! 첫번째
-  let kimochi : string = "기모찌";
-  let arr : string[] = [];
-  arr.push(kimochi);
-  return arr;
+  // // ! 첫번째
+  // // let kimochi : string = "기모찌";
+  // // let arr : string[] = [];
+  // // arr.push(kimochi);
+  // // // return arr.push(kimochi);
+  // // return arr;
+  // // ! 두번째
+  // // let susi : string = "기모링";
+  // let arr :string[] = [];
+  // let result : string[] = arr.map(a => {
+  //   a + "하이"
+  // })
+  // return result;
+  // * 1
+  // const insertString :string = 
+  // * 2 
+  arrayData.pop();
+  // * 3
+  let combineArray : string[] = [];
+  // * 4
+  combineArray = [insertString, ...arrayData];
+  // * 5
+  return combineArray;
 }
 // 기존 배열에 0번째를 최종적으로 바꿔주는 형태
 
@@ -33,4 +51,5 @@ function first(arrayData : string[]) : string[] {
 // - map()
 // - push()
 let data = ['최대건','정성철','허진','이세민'];
-console.log(first(data))
+console.log(first(data,"대머리"))
+// console.log(...data)
